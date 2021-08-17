7fb5d029cd2da95797572f7188f85ff314c4e401
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-17 20:42:18 +0530

    Update __main__.py

--- ef_norm/__main__.py
+++ ef_norm/__main__.py
@@ -61,7 +61,6 @@ from ef_norm.modules.helper_funcs.alternate import typing_action
 from ef_norm.modules.helper_funcs.chat_status import is_user_admin
 from ef_norm.modules.helper_funcs.misc import paginate_modules
 from ef_norm.modules.helper_funcs.readable_time import get_readable_tim
-from ef_norm.plugins import *
 
 PM_START_TEXT = """
 Hello there, I'm Norm
